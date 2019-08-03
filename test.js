f3bb7618abeec7b30866105cc099c0b836fdd3df
MobiusStudios <52831647+MobiusStudios@users.noreply.github.com>
2019-08-03 14:11:13 +0100

    added varibles to test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const rbx = require("noblox.js")
 const settings = "./settings.json"
-
-
+const cookie = settings.cookie
+const token = settings.token
 async function startApp () {
     await rbx.cookieLogin()
     // Do everything else, calling functions and the like.
